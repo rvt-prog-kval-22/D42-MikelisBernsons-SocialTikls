13fe16a41deff7727355844910c14381b2b4f3e0
MKBernsons <mkbernsons@gmail.com>
2022-01-15 20:08:42 +0200

    Add project files.

---
+++ BredWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BredWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BredWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
